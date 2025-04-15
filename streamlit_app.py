8872058552607b5fa6b78eb9b072710594a29812
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2025-04-15 10:35:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,18 @@ with open('model.pkl','rb') as f:
 #title the page
 st.title("predict the CO2 EMISSIONS of car")
 
-#set image
-st.image('/content/drive/MyDrive/Colab Notebooks/Capture1.PNG')
 
 #inputs
 engine_size = st.number_input('Engine Size' , min_value=0.0 , max_value=10.0,value=1.0)
 cylinder = st.number_input('Cylinder' , min_value=0.0 , max_value=10.0,value=1.0)
-fuel_consumption =  st.number_input('Fuel Consumption' , min_value=0.0 , max_value=100.0,value=1.0)
+FUELCONSUMPTION_CITY =  st.number_input('FUEL CONSUMPTION CITY' , min_value=0.0 , max_value=100.0,value=1.0)
+FUELCONSUMPTION_HWY = st.number_input('FUEL CONSUMPTION HWY' , min_value=0.0 , max_value=10.0,value=1.0)
+FUELCONSUMPTION_COMB = st.number_input('FUEL CONSUMPTION COMB' , min_value=0.0 , max_value=10.0,value=1.0)
+FUELCONSUMPTION_COMB_MPG =  st.number_input('FUEL CONSUMPTION COMB_MPG' , min_value=0.0 , max_value=100.0,value=1.0)
 
-output = model.predict([[engine_size,cylinder,fuel_consumption]])
+
+
+output = model.predict([[engine_size, cylinder, FUELCONSUMPTION_CITY,FUELCONSUMPTION_HWY, FUELCONSUMPTION_COMB,FUELCONSUMPTION_COMB_MPG]])
 
 #display the result
 st.write("the predict CO2 of car is : ",round(output[0][0],2))
